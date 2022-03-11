b3695257d59c7b96ad8ecd7e85ef8f1639264b92
mbt371 <57286135+mbt371@users.noreply.github.com>
2022-03-12 02:24:00 +0300

    commit message for 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,10 +12,16 @@ int main(void) /* main - assign numbers to variables */
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	if (n > 0)
+		{
 		printf("%d is positive\n", n);
+		}
 	else if (n < 0)
+		{
 		printf("%d is negative\n", n);
+		}
 	else
+		{
 		printf("%d is zero\n", n);
+		}
 	return (0);
 }
