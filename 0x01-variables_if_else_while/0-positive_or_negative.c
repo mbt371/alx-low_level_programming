3c46778cbd7327a707b67a88e0cd9eb61f162a64
mbt371 <57286135+mbt371@users.noreply.github.com>
2022-03-12 02:05:19 +0300

    commit message for 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 /** more headers goes there */
-
+/* main -assign random number to the variable n */
 /* betty style doc for function main goes there */
 int main(void)
 {
@@ -12,10 +12,10 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	if (n > 0)
-		printf("%d is positive", n);
+		printf("%d is positive\n", n);
 	else if (n < 0)
-		printf("%d is negative", n);
+		printf("%d is negative\n", n);
 	else
-		printf("%d  is zero", n);
+		printf("%d  is zero\n", n);
 	return (0);
 }
