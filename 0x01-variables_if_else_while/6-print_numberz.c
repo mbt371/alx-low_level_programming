0ac6751405219d0c7521e954db6c3cb5bddae78e
mbt371 <57286135+mbt371@users.noreply.github.com>
2022-03-12 03:05:45 +0300

    commit message to 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - prints single digit numbers
+* Return: Always (Success)
+*
+*/
+int main (void)
+{
+	int i;
+
+	for (i=0; i<10; i++)
+	{
+		printf("%d",i);
+	}
+	return (0);
+}
