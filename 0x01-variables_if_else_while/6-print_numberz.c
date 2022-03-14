ccbf22a91eb5b096e4a31b78d18586edda9273e9
mbt371 <57286135+mbt371@users.noreply.github.com>
2022-03-14 23:46:28 +0300

    commit for numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,10 +8,11 @@ int main(void)
 
 {
 int i;
-for (i = 48; i < 57; i++)
-{putchar (i);
+for (i = 48; i < 58; i++)
+{
+putchar (i) ;
 }
 
-putchar ('\n');
-return (0);
+putchar ('\n') ;
+return (0) ;
 }
