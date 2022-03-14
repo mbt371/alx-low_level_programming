5a9092aad8734b13a0ed860cce7315533db16e9b
mbt371 <57286135+mbt371@users.noreply.github.com>
2022-03-14 22:29:49 +0300

    commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,14 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/** more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+* main - Entry point
+*
+* Return: Always 0 (Sucess)
+* betty style doc for function main goes there 
+*/
+
 int main(void)/* maiin assigns numbers to variable n */
 {
 	int n;
