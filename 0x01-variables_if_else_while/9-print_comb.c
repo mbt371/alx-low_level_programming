97e90053d767cbdb02cc3e6a65e921696b34f043
mbt371 <57286135+mbt371@users.noreply.github.com>
2022-03-15 00:51:09 +0300

    commit to task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 /**
-*
 * main - entry point
+*
 * Return: Always 0
 */
-int main(void)
+int main (void)
 
 {
 int number;
 
-for (number =48; number <= 57; number++)
+for (number = 48; number <= 57; number++)
 {
 putchar(number);
 
