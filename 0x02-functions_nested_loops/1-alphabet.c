d9486a499a7d043a4dc80133ec2dce3dbb45ebd2
mbt371 <57286135+mbt371@users.noreply.github.com>
2022-03-15 20:33:31 +0300

    commit message to print aphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* print aphabet
+*
+* Return: void
+*
+*/
+void print_alphabet(void)
+{
+char letter = 'a';
+
+while (letter <= 'z')
+{
+_putchar(letter);
+letter++;
+}
+_putchar('\n');
+}
