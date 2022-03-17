41948f342ca6799a04de14bbad97c70067a00af7
mbt371 <57286135+mbt371@users.noreply.github.com>
2022-03-18 00:05:03 +0300

    commit

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -3,7 +3,7 @@
 /**
  * print_triangle - entry point
  *
- * Description: prints diagonal 
+ * Description: prints diagonal
  *@size : size of the triangle
  * Return: void
  */
@@ -18,7 +18,7 @@ void print_triangle(int size)
 	}
 	else
 	{
-		for (row =1; row <= size; row++)
+		for (row = 1; row <= size; row++)
 		{
 			for (spaces = size - row; spaces >= 1; spaces--)
 			{
