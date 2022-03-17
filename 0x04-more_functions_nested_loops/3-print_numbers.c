b6471848274cf08c5bc523b713f238b5ace812b5
mbt371 <57286135+mbt371@users.noreply.github.com>
2022-03-17 22:43:52 +0300

    commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - priint the numbers from 0 t0 9
+ * Description: you can only use _putchar twice
+ */
+
+void print_numbers(void)
+{
+int numbers;
+
+numbers = 0;
+while (numbers < 10)
+{
+_putchar(numbers + '0');
+numbers++;
+}
+_putchar('\n);
+}
