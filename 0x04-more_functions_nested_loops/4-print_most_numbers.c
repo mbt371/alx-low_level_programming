016d6e51d32248b28a2edbffe94d6d2f9e591fb8
mbt371 <57286135+mbt371@users.noreply.github.com>
2022-03-18 21:38:59 +0300

    commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,20 +2,25 @@
 
 /**
  * print_most_numbers - priints numbers
- * Return: 0
+ *
+ * Return: Always 0
  */
 
 void print_most_numbers(void)
 {
-	char n = 0;
 
-	while (n <= 9)
-	{
-		if (n != 2 && n != 4)
-		{
-			_putchar('0' + n);
-		}
-		n++;
-	}
-	_putchar('\n');
+int i;
+
+i = 0;
+while (i < 10)
+{
+if (i != 2 && i != 4)
+{
+_putchar (i + '0');
 }
+i++;
+}
+
+_putchar ('\n');
+}
+
