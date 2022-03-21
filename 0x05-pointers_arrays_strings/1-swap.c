94672950e901cb6133f182c90e296a910234093b
mbt371 <57286135+mbt371@users.noreply.github.com>
2022-03-21 21:56:54 +0300

    commit

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *swap_int - swap numbers function
+ *@a: integer
+ *@b: integer
+ *
+ *Return: nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+int temp;
+
+temp = *a;
+*a = *b;
+*b = temp;
+}
