3c436421fa1078fca636d8a69154516de429e8b5
mbt371 <57286135+mbt371@users.noreply.github.com>
2022-03-23 22:06:11 +0300

    commit

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,5 @@
+#ifndef _main_h_
+#define _main_h_
 int _putchar(char c);
 int _strlen(char *s);
 char *_strcat(char *dest, char *src);
@@ -13,3 +15,5 @@ char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
+
+#endif
