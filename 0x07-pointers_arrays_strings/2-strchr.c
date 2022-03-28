1d7b9189845903b60fd92ee46cda33aa59942056
mbt371 <57286135+mbt371@users.noreply.github.com>
2022-03-28 22:25:09 +0300

    commit to 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,10 +1,10 @@
 /*
 * File: 2-strchr.c
-* Author: Rodrigo Zárate Algecira
-* Date: Monday 06 july 2021
+* Author: mbt371
+* Date: Monday 28 march 2022
 */
 
-#include "holberton.h"
+#include "main.h"
 
 /**
 * _strchr - Find character in string.
@@ -25,4 +25,4 @@ char *_strchr(char *s, char c)
 
 return (s);
 }
-[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[C[C[C[C[B[C[C[C[C[C[Cmbt371[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~ [3~[3~[3[B[C[C[C[C[C[C[C[C[C[C[2[D[D[D[march[D[D[D[D[D[C[C[C[C[C [D[D[D[D[D[D28[B[B[Bmain.h[C[C[C[C"[B[B[B[B[B[B[B[B[B[B[B[B[B[B[
+
