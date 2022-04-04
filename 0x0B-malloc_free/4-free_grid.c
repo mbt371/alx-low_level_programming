4685a8b2dd3b5a79a793d1bf3955558502e300d0
mbt371 <57286135+mbt371@users.noreply.github.com>
2022-04-04 14:38:58 +0300

    commit to 4-free_grid.c

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,27 @@
+/*
+* 4-free_grid.c - exercise to try malloc
+* Author: mbt371
+* Date: Juy 13 2021
+*/
+
+#include "main.h"
+#include <stdlib.h>
+/**
+* free_grid - Frees memry of the array
+* @height: height of array
+* @grid: the array itself
+* Return: void
+*/
+
+void free_grid(int **grid, int height)
+{
+	int x;
+	/* walk the grid */
+	for (x = 0; x < height; x++)
+	{
+		free(grid[x]);
+	}
+	/* clear all */
+	free(grid);
+}
+
