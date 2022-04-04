61659913bcfca6605504ec524ea15b057c26905d
mbt371 <57286135+mbt371@users.noreply.github.com>
2022-04-04 14:32:53 +0300

    commit to 2-str_concat.c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,56 @@
+/*
+* 2-str_concat.c - exercise to join two strings using malloc
+* Author: mbt371
+* Date: 4 April 2022
+*/
+
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* str_concat - Join  tow strings
+* @s1: string one
+* @s2: string two
+* Return: NULL or a pointer
+*/
+
+char *str_concat(char *s1, char *s2)
+{
+char *thereturnedpointer;
+int lengthone = 0;
+int lengthtwo = 0;
+
+int i, x;
+	if (s1 == NULL)
+		lengthone = 0;
+	else
+	{
+		for (i = 0; s1[i]; i++)
+			lengthone++;
+	}
+
+	if (s2 == NULL)
+		lengthtwo = 0;
+	else
+	{
+		for (i = 0; s2[i]; i++)
+			lengthtwo++;
+	}
+	x = lengthone + lengthtwo + 1;
+	thereturnedpointer = malloc(x * sizeof(char));
+	if (thereturnedpointer == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < lengthone; i++)
+	{
+		thereturnedpointer[i] = s1[i];
+	}
+	for (i = 0; i < lengthtwo; i++)
+	{
+		thereturnedpointer[i + lengthone] = s2[i];
+	}
+	thereturnedpointer[lengthone + lengthtwo] = '\0';
+return (thereturnedpointer);
+}
+
