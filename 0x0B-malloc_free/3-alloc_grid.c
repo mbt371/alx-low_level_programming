26660592c55081c18c55983446a4b97cf28045df
mbt371 <57286135+mbt371@users.noreply.github.com>
2022-04-04 14:36:30 +0300

    commit to 3-alloc_grid.c

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,51 @@
+/*
+* 3-alloc_grid.c - double pointer to matrix
+* Author: mbt371
+* Date: 4 April 2022
+*/
+
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* alloc_grid - Creates an array
+* @width: width
+* @height: height
+* Return: NULL or a pointer
+*/
+
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **a;
+	/* case w or h = NULL */
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	/* set memory */
+	a = (int **)malloc(sizeof(int *) * height);
+	/* if malloc fails ret NULL */
+	if (a == NULL)
+		return (NULL);
+	/* walk the grid */
+	for (i = 0; i < height; i++)
+	{
+		a[i] = (int *)malloc(sizeof(int) * width);
+		/* if malloc fails free memry */
+		if (a[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+				free(a[j]);
+			free(a);
+			return (NULL);
+		}
+		/* populate */
+		for (j = 0; j < width; j++)
+		{
+			a[i][j] = 0;
+		}
+	}
+	return (a);
+}
+
