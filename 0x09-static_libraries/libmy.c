4556c8918f9161a308093834200a99fde22a3256
mbt371 <57286135+mbt371@users.noreply.github.com>
2022-04-06 10:00:24 +0300

    commit

--- 0x09-static_libraries/libmy.c
+++ 0x09-static_libraries/libmy.c
@@ -1,7 +1,4 @@
 #!/bin/bash
-gcc -Wall -c *.c
-ar -qcv liball.a *.o
-
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -22,4 +19,3 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
