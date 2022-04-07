9f6b357d8ab9de36426b8c8c4a7f0a7377bf6ec4
mbt371 <57286135+mbt371@users.noreply.github.com>
2022-04-07 09:05:43 +0300

    commit to 2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,47 @@
+/*
+* 2-calloc.c - emulate calloc function
+* Author: mbt371
+* Date: April 7 2022
+*/
+
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+* _calloc - shows the basic function of calloc
+* @nmemb: unsigned int
+* @size: unsigned int
+* Return: void pointer or NULL
+*/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+/* declarations on top REMEMBER mufasa */
+char *p;
+/* unsigned to compare unsigned */
+unsigned int i;
+/* If nmemb or size is 0, then _calloc returns NULL */
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+p = malloc(size * nmemb);
+
+	if (p == NULL)
+	{
+	/* memory allocation fails */
+		return (NULL);
+	}
+
+	for (i = 0; i < nmemb * size; i++)
+	{
+		p[i] = '\0';
+	}
+
+/* return the pointer */
+return (p);
+}
+
