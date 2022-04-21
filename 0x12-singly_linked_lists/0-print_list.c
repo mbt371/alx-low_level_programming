339f4cb4dd42a6c8948c918d3c2e8400177cffb3
mbt371 <57286135+mbt371@users.noreply.github.com>
2022-04-21 21:11:52 +0300

    commit to 0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,37 @@
+/*
+* 0-print_list.c - Print list
+* Author: mbt371
+* Date: April 21, 2022
+*/
+
+#include "lists.h"
+
+/**
+* print_list - Prints elements in list
+* @h: pointer to const list
+* Return: number of nodes
+*/
+
+size_t print_list(const list_t *h)
+{
+
+int i;
+const list_t *dt = h;
+size_t howmany = 0;
+/* walk list */
+	for (i = 0; dt; i++)
+	{
+		if (dt->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", dt->len, dt->str);
+		}
+	dt = dt->next;
+	howmany++;
+	}
+return (howmany);
+}
+
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+readme to 0x12-singly_linked_lists
