21a43102e52acb6c520800c96afbc02078e3d3f3
mbt371 <57286135+mbt371@users.noreply.github.com>
2022-04-21 21:19:53 +0300

    commit to 3-add_node_end.c

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,64 @@
+/*
+* 3-add_node_end.c - Add node at the end
+* Author: mbt371
+* Date: April 21, 2022
+*/
+
+#include "lists.h"
+
+/**
+* _slenght - lenght
+* @c: char
+* Return: int
+*/
+unsigned int _slenght(char *c)
+{
+int i;
+/* walk the string */
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		continue;
+	}
+return (i);
+}
+/**
+* add_node_end - Node in end
+* @head: Head node
+* @str: string
+* Return: new node
+*/
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *newnode, *lastnode;
+
+	if (str == NULL)
+		return (NULL);
+	newnode = malloc(sizeof(list_t));
+	if (newnode == NULL)
+	{
+		return (NULL);
+	}
+	newnode->str = strdup(str);
+	if (newnode->str == NULL)
+	{
+		free(newnode);
+		return (NULL);
+	}
+	newnode->len = _slenght(newnode->str);
+	newnode->next = NULL;
+	if (*head == NULL)
+	{
+		*head = newnode;
+	}
+	else
+	{
+		lastnode = *head;
+		while (lastnode->next)
+		{
+			lastnode = lastnode->next;
+		}
+	lastnode->next = newnode;
+	}
+return (newnode);
+}
+
