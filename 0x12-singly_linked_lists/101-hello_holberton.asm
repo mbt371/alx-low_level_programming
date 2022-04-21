f08da64558196be751992a5a1b59a10a52d86d3e
mbt371 <57286135+mbt371@users.noreply.github.com>
2022-04-21 21:34:43 +0300

    commit to 101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,30 +1,23 @@
-;--------------------------------------------------------------------------------------
-;
-; A 64-bit program in assembly that prints Hello, Holberton, followed by a new line.
-; Compiled using nasm and gcc.
-; nasm -f elf64 101-hello_holberton.asm && gcc 101-hello_holberton.o -o hello
-;
-;--------------------------------------------------------------------------------------
+; Declare needed C  functions
+	extern	printf		; the C function, to be called
 
-section .data
-format db "%s", 10, 0
-message db "Hello, Holberton", 0
+	section .data		; Data section, initialized variables
+msg:	db "Hello, Holberton", 0; C string needs 0
+fmt:	db "%s", 10, 0		; The printf format, "\n",'0'
 
-section .text
-extern printf ;C function to be called
-global main ;main function
+	section .text		; Code section.
 
-main:
-	push rbp
-	mov rdi, format
-	mov rsi, message
-	mov rax, 0 ;can be xor rax, rax
-	call printf ;call C function.
+	global main		; the standard gcc entry point
+main:				; the program label for the entry point
+	push	rbp		; set up stack frame, must be alligned
 
-	pop rbp ;restor stack
+	mov	rdi,fmt
+	mov	rsi,msg
+	mov	rax,0		; or can be  xor  rax,rax
+	call	printf		; Call C function
 
-	mov rax, 0 ;normal, no error, return value
-	ret ;return
-© 2022 GitHub, Inc.
-Terms
+	pop	rbp		; restore stack
+
+	mov	rax,0		; normal, no error, return value
+	ret			; return
 
