bd28b9af6a506d0fa9d068dbec09e1f45fd31592
mbt371 <57286135+mbt371@users.noreply.github.com>
2022-04-21 21:28:54 +0300

    commit to 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,23 @@
-#include "lists.h"
+#include <stdio.h>
+/*
+ * Apply the constructor attribute to startupfun()
+ * so that it is executed before main()
+*/
+	void startupfun(void) __attribute__ ((constructor));
+
+/*
+ * Apply the destructor attribute to cleanupfun()
+ *so that it is executed after main()
+ */
+
+	void cleanupfun(void)__attribute__ ((destructor));
 /**
-  *print - prints before main
-  *
-  *Return: void.
-  */
-void print(void)
-{
-	printf("You're beat! and yet, you must allow,");
-	printf("\nI bore my house upon my back!\n");
-}
+ * startupfun - This functions print a text
+ *
+ */
+	void startupfun(void)
+	{
+		printf("You're beat! and yet, you must allow,");
+		printf("\nI bore my house upon my back!\n");
+	}
 
