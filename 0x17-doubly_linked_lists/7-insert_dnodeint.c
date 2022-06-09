282e5f1ceba636fddc4079c2690a9e3fb81ceedc
mbt371 <57286135+mbt371@users.noreply.github.com>
2022-06-09 22:41:14 +0300

    commit to 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,40 +1,46 @@
+/*
+ * File: 7-insert_dnodeint.c
+ * Auth: mbt371
+ */
+
 #include "lists.h"
+
 /**
- * insert_dnodeint_at_index - inserts a new node at a given position
- * @h: pointer to the head pointer of the list
- * @idx: index of the list where the new node should be added
- * @n: n-node
- * Return: the address of the new node, or NULL if it failed
- * Description: inserts a new node at a given position
+ * insert_dnodeint_at_index - Inserts a new node in a dlistint_t
+ *                            list at a given position.
+ * @h: A pointer to the head of the dlistint_t list.
+ * @idx: The position to insert the new node.
+ * @n: The integer for the new node to contain.
+ *
+ * Return: If the function fails - NULL.
+ *         Otherwise - the address of the new node.
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int node = 0;
-	dlistint_t *prev, *new;
+	dlistint_t *tmp = *h, *new;
 
-	if (h == NULL)
-		return (NULL);
 	if (idx == 0)
-		return (add_dnodeint_end(h, n));
-	if (*h == NULL)
-		return (NULL);
-	prev = *h;
-	for (node = 0; node < idx - 1; node++)
+		return (add_dnodeint(h, n));
+
+	for (; idx != 1; idx--)
 	{
-		/*si idx es mayor que la lista*/
-		if (prev == NULL)
-		{
+		tmp = tmp->next;
+		if (tmp == NULL)
 			return (NULL);
-		}
-		prev = prev->next;
 	}
+
+	if (tmp->next == NULL)
+		return (add_dnodeint_end(h, n));
+
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
+
 	new->n = n;
-	new->prev = prev;
-	new->next = prev->next;
-	prev->next->prev = new;
-	prev->next = new;
+	new->prev = tmp;
+	new->next = tmp->next;
+	tmp->next->prev = new;
+	tmp->next = new;
+
 	return (new);
 }
